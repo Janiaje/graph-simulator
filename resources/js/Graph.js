16dc4821ed76e89735d45a70ff9050efabc030af
Janiaje <tihanyi.janos@gmail.com>
2019-07-20 17:15:13 +0200

    thesis-1 Graph export/import - Edge/node list + CSV

--- resources/js/Graph.js
+++ resources/js/Graph.js
@@ -15,7 +15,20 @@ class Graph {
         this._network = new vis.Network(container, {
             nodes: this._nodes,
             edges: this._edges
-        }, this._options)
+        }, this._options);
+
+        // Parser part
+
+        this._exportNodeListFormat = [
+            'id',
+            'label',
+        ];
+
+        this._exportEdgeListFormat = [
+            'id',
+            'from',
+            'to',
+        ];
     }
 
     /**
@@ -27,7 +40,7 @@ class Graph {
     }
 
     /**
-     * Generate random graph
+     * Generate random graph.
      */
     generateRandomGraph(fullGraph) {
         this.clearGraph();
@@ -52,7 +65,9 @@ class Graph {
     }
 
     /**
-     * Generate edges for full graph
+     * Generate edges for full graph.
+     *
+     * @param numberOfNodes int
      */
     _generateEdgesForFullGraph(numberOfNodes) {
         let edges = [];
@@ -73,11 +88,114 @@ class Graph {
     }
 
     /**
-     * Create range (array containing all the numbers from-to the given parameters)
+     * Create range (array containing all the numbers from-to the given parameters).
+     *
+     * @param from int
+     * @param to int
+     *
+     * @returns {Array.<int>}
      */
     _range(from, to) {
         return [...Array(to - from + 1).keys()]
             .map(value => value + from)
+    }
+
+
+    // Parser part
+
+    /**
+     * Returns a nodeList with only the properties specified in the _exportNodeListFormat list.
+     *
+     * @param propertyNamesArray Array.String
+     * @param arrayOfObjects Array.<Object>
+     *
+     * @returns {Array.<Object>}
+     */
+    _formatObjectList(propertyNamesArray, arrayOfObjects) {
+        return arrayOfObjects.map((object) => {
+            let newObject = {};
+
+            propertyNamesArray.forEach((key) => {
+                newObject[key] = object[key];
+            });
+
+            return newObject;
+        });
+    }
+
+    /**
+     * Returns a nodeList with only the properties specified in the _exportNodeListFormat list.
+     *
+     * @returns {Array.<Object>}
+     */
+    _getExportNodeList() {
+        return this._formatObjectList(this._exportNodeListFormat, this._nodes.get());
+    }
+
+    /**
+     * Returns an edgeList with only the properties specified in the _exportEdgeListFormat list.
+     *
+     * @returns {Array.<Object>}
+     */
+    _getExportEdgeList() {
+        return this._formatObjectList(this._exportEdgeListFormat, this._edges.get());
+    }
+
+    /**
+     * Returns an node list
+     *
+     * @returns {Array.<Object>}
+     */
+    exportNodeList() {
+        return this._getExportNodeList();
+    }
+
+    /**
+     * Returns an edge list
+     *
+     * @returns {Array.<Object>}
+     */
+    exportEdgeList() {
+        return this._getExportEdgeList();
+    }
+
+    _createDataFile(separator, header, rows) {
+        rows = rows.map((node) => {
+            let row = [];
+
+            header.forEach((key) => {
+                row.push(node[key]);
+            });
+
+            row = row.join(separator);
+
+            return row;
+        });
+
+        header = header.join(separator);
+
+        let csv = [header].concat(rows);
+        csv = csv.join("\n");
+
+        return csv;
+    }
+
+    /**
+     * Returns CSV representing the nodes of the graph
+     *
+     * @returns {string}
+     */
+    exportNodeCSV() {
+        return this._createDataFile(",", this._exportNodeListFormat, this._getExportNodeList());
+    }
+
+    /**
+     * Returns CSV representing the nodes of the graph
+     *
+     * @returns {string}
+     */
+    exportEdgeCSV() {
+        return this._createDataFile(",", this._exportEdgeListFormat, this._getExportEdgeList());
     }
 }
 
