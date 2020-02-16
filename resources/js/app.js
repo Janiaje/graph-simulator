ea81b7b8ebab4763d26014af4d808464a522f5e5
janiaje <tihanyi.janos@gmail.com>
2020-02-16 14:47:28 +0100

    Fix chart x axis overlapping labels

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,8 +8,6 @@
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {fas} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-// Import ApexCharts
-import VueApexCharts from 'vue-apexcharts'
 // Import Analyses
 import AverageDegreeAnalysis from "./analysis/analyses/AverageDegreeAnalysis";
 // Import Simulations
@@ -38,7 +36,6 @@ files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(
 // Register external components
 library.add(fas);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
-Vue.component('apexchart', VueApexCharts);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- resources/js/components/simulation/SimulationChart.vue
+++ resources/js/components/simulation/SimulationChart.vue
@@ -1,16 +1,19 @@
 <template>
     <div id="simulation-chart" class="inner-window" v-show="show">
         <div id="simulation-chart-inside">
-            <apexchart width="500" type="line" :width="'100%'" :options="options" :series="series"/>
+            <div id="apexChart"></div>
         </div>
     </div>
 </template>
 
 <script>
+    import ApexCharts from 'apexcharts';
+
     export default {
         name: "SimulationDiagram",
         data() {
             return {
+                chart: null,
                 show: false,
                 showCallback: () => {
                     this.show = mainDisplayedGraph.simulation.displayLineChart();
@@ -26,11 +29,11 @@
                             show: false,
                             // TODO: zoom and other options
                         }
-                    }
+                    },
+                    series: [],
                 },
-                series: [],
                 stepChangedListener: (data) => {
-                    this.series = data.series;
+                    this.chart.updateSeries(data.series);
                 }
             }
         },
@@ -39,12 +42,17 @@
             eventHub.$on('simulation-loaded', this.showCallback);
             eventHub.$on('simulation-ended', this.hideCallback);
             eventHub.$on('simulation-step-changed', this.stepChangedListener);
+
+            this.chart = new ApexCharts(document.querySelector("#apexChart"), this.options);
+            this.chart.render();
         },
 
         destroyed() {
             eventHub.$off('simulation-loaded', this.showCallback);
             eventHub.$off('simulation-ended', this.hideCallback);
             eventHub.$off('simulation-step-changed', this.stepChangedListener);
+
+            this.chart.destroy()
         }
     }
 </script>
